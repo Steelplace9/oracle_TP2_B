d5ab619e95f7d5b5b0b90483a039f8789be8994a
Steelplace9 <vincentdenommeeleblanc@gmail.com>
2025-05-19 16:07:31 -0400

    ajout des test du package gestion membres securises

---
+++ test_package.sql
@@ -0,0 +1,37 @@
+DECLARE
+  v_id_membre     MEMBRES.ID_Membre%TYPE;
+  v_nouv_courriel  VARCHAR2(200) := 'nouveau.imane@garneau.ca';
+  v_nouv_tel      VARCHAR2(20) := '418-222-1111';
+  v_courriel_chiffre  VARCHAR2(1000);
+  v_tel_chiffre       VARCHAR2(1000);
+BEGIN
+
+  FOR membre IN (SELECT ID_Membre FROM Membres) LOOP
+    pkg_gestion_securisee_des_membres.copier_infos_chiffrees(membre.ID_Membre);
+  END LOOP;
+
+  v_id_membre := pkg_gestion_securisee_des_membres.ajouter_membre('Meziane', 'Imane', 'imane@garneau.ca','418-111-2222', SYSDATE, 1);
+
+  pkg_gestion_securisee_des_membres.copier_infos_chiffrees(v_id_membre);
+
+  pkg_gestion_securisee_des_membres.modifier_coordonnees_membre(v_id_membre, 'COURRIEL', v_nouv_courriel);
+
+  pkg_gestion_securisee_des_membres.modifier_coordonnees_membre(v_id_membre, 'TELEPHONE', v_nouv_tel);
+
+  v_courriel_chiffre := pkg_gestion_securisee_des_membres.get_coordonnees_dechiffrees(v_id_membre, 'COURRIEL');
+
+  DBMS_OUTPUT.PUT_LINE('Courriel déchiffré : ' || v_courriel_chiffre);
+
+  v_tel_chiffre := pkg_gestion_securisee_des_membres.get_coordonnees_dechiffrees(v_id_membre, 'TELEPHONE');
+
+  DBMS_OUTPUT.PUT_LINE('Téléphone déchiffré : ' || v_tel_chiffre);
+
+  pkg_gestion_securisee_des_membres.archiver_membre_et_dependances(v_id_membre);
+
+  DELETE MEMBRES WHERE ID_MEMBRE = v_id_membre;
+  DELETE MEMBRES_SECURISES WHERE ID_MEMBRE = v_id_membre;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Erreur : ' || SQLERRM);
+END;
