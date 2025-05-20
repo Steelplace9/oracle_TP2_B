66a4870c53743f4ca3fdfa3b985881ac3497f723
Steelplace9 <vincentdenommeeleblanc@gmail.com>
2025-05-19 21:58:22 -0400

    ajout des transactions

--- pkg_gestion_membres_securises.sql
+++ pkg_gestion_membres_securises.sql
@@ -6,15 +6,15 @@ CREATE OR REPLACE PACKAGE pkg_gestion_securisee_des_membres AS
     );
 
     TYPE r_membre_chiffre_type IS RECORD (
-        couriel MEMBRES.EMAIL_MEMBRE%TYPE,
-        telephone MEMBRES.TELEPHONE_MEMBRE%TYPE
+        couriel MEMBRES_SECURISES.COURRIEL_CHIFFRE%TYPE,
+        telephone MEMBRES_SECURISES.TELEPHONE_CHIFFRE%TYPE
     );
 
     TYPE ref_membre IS REF CURSOR;
 
-    TYPE t_resevations_type IS TABLE OF RESERVATIONS%ROWTYPE;
+    TYPE t_resevations_type IS TABLE OF RESERVATIONS_ARCHIVEES%ROWTYPE;
 
-    TYPE t_paiements_type IS TABLE OF PAIMENTS%ROWTYPE;
+    TYPE t_paiements_type IS TABLE OF PAIEMENTS_ARCHIVES%ROWTYPE;
 
     PROCEDURE copier_infos_chiffrees(p_id_membre MEMBRES.ID_MEMBRE%TYPE);
 
@@ -55,7 +55,8 @@ CREATE OR REPLACE PACKAGE BODY pkg_gestion_securisee_des_membres AS
         COMMIT;
     EXCEPTION
         WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('une erreur est survenue l"aure de l"execution de la procedure copier_infos_chiffrees');
+            ROLLBACK;
+            DBMS_OUTPUT.PUT_LINE('une erreur est survenue l"aure de l"execution de la procedure copier_infos_chiffrees' || SQLERRM);
     END copier_infos_chiffrees;
 
     FUNCTION ajouter_membre(p_nom MEMBRES.NOM_MEMBRE%type, p_prenom MEMBRES.PRENOM_MEMBRE%type, p_couriel MEMBRES.EMAIL_MEMBRE%type, p_telephone MEMBRES.TELEPHONE_MEMBRE%type, p_date MEMBRES.DATE_INSCRIPTION%type, p_id_abonement MEMBRES.ID_ABONNEMENT%type) return MEMBRES.ID_MEMBRE%type IS
@@ -65,26 +66,31 @@ CREATE OR REPLACE PACKAGE BODY pkg_gestion_securisee_des_membres AS
         RETURN SEQ_GENERATEUR_ID.currval;
     EXCEPTION
         WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('une erreur est survenue l"aure de l"execution de la procedure ajouter_membre');
+            DBMS_OUTPUT.PUT_LINE('une erreur est survenue l"aure de l"execution de la procedure ajouter_membre' || SQLERRM);
     END ajouter_membre;
 
     PROCEDURE modifier_coordonnees_membre (p_champ_a_modifier varchar2, p_id_membre MEMBRES.ID_MEMBRE%type, p_champ_modifier varchar2) IS
     BEGIN
-        IF p_champ_a_modifier = 'courriel' THEN
+        SAVEPOINT avant_modification_membre;
+
+        IF p_champ_a_modifier = 'COURRIEL' THEN
             UPDATE MEMBRES SET EMAIL_MEMBRE = p_champ_modifier WHERE ID_MEMBRE = p_id_membre;
             UPDATE MEMBRES_SECURISES SET COURRIEL_CHIFFRE = PKG_CHIFFREMENT.CHIFFRER(p_champ_modifier) WHERE ID_MEMBRE = p_id_membre;
-        ELSIF p_champ_a_modifier = 'telephone' THEN
+        ELSIF p_champ_a_modifier = 'TELEPHONE' THEN
             UPDATE MEMBRES SET TELEPHONE_MEMBRE = p_champ_modifier WHERE ID_MEMBRE = p_id_membre;
             UPDATE MEMBRES_SECURISES SET TELEPHONE_CHIFFRE = PKG_CHIFFREMENT.CHIFFRER(p_champ_modifier) WHERE ID_MEMBRE = p_id_membre;
         ELSE
+            ROLLBACK TO SAVEPOINT avant_modification_membre;
             RAISE_APPLICATION_ERROR(-20002,'Erreur : Champ à modifier non reconnu');
         end if;
         COMMIT;
     EXCEPTION
         WHEN NO_DATA_FOUND THEN
+            ROLLBACK TO SAVEPOINT avant_modification_membre;
             DBMS_OUTPUT.PUT_LINE('aucune information n''a pas été trouver avec l''id donner');
         WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('une erreur est survenue l"aure de l"execution de la procedure modifier_coordonnees_membre');
+            ROLLBACK;
+            DBMS_OUTPUT.PUT_LINE('une erreur est survenue l"aure de l"execution de la procedure modifier_coordonnees_membre' || SQLERRM);
     END modifier_coordonnees_membre;
 
     FUNCTION get_coordonnees_dechiffrees(p_champ_a_modifier varchar2, p_id_membre MEMBRES.ID_MEMBRE%type) return varchar2 IS
@@ -92,16 +98,16 @@ CREATE OR REPLACE PACKAGE BODY pkg_gestion_securisee_des_membres AS
         v_chiffre VARCHAR2(1000);
         v_claire VARCHAR2(100);
     BEGIN
-        IF p_champ_a_modifier = 'courriel' THEN
+        IF p_champ_a_modifier = 'COURRIEL' THEN
             OPEN c_info FOR
-                SELECT EMAIL_MEMBRE
-                FROM MEMBRES
-                WHERE MEMBRES.ID_MEMBRE = p_id_membre;
-        ELSIF p_champ_a_modifier = 'telephone' THEN
+                SELECT COURRIEL_CHIFFRE
+                FROM MEMBRES_SECURISES
+                WHERE MEMBRES_SECURISES.ID_MEMBRE = p_id_membre;
+        ELSIF p_champ_a_modifier = 'TELEPHONE' THEN
             OPEN c_info FOR
-                SELECT TELEPHONE_MEMBRE
-                FROM MEMBRES
-                WHERE MEMBRES.ID_MEMBRE = p_id_membre;
+                SELECT TELEPHONE_CHIFFRE
+                FROM MEMBRES_SECURISES
+                WHERE MEMBRES_SECURISES.ID_MEMBRE = p_id_membre;
         ELSE
             RAISE_APPLICATION_ERROR(-20003, 'Type de champ invalide (courriel ou telephone attendu)');
         END IF;
@@ -116,7 +122,7 @@ CREATE OR REPLACE PACKAGE BODY pkg_gestion_securisee_des_membres AS
         WHEN NO_DATA_FOUND THEN
             DBMS_OUTPUT.PUT_LINE('aucune information n''a pas été trouver avec l''id donner');
         WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('une erreur est survenue l"aure de l"execution de la procedure get_coordonnees_dechiffrees');
+            DBMS_OUTPUT.PUT_LINE('une erreur est survenue l"aure de l"execution de la procedure get_coordonnees_dechiffrees' || SQLERRM);
     END get_coordonnees_dechiffrees;
 
     PROCEDURE archiver_membre_et_dependances(p_id_membre MEMBRES.ID_MEMBRE%type) IS
@@ -124,34 +130,38 @@ CREATE OR REPLACE PACKAGE BODY pkg_gestion_securisee_des_membres AS
         t_paiements t_paiements_type;
         v_membre MEMBRES%rowtype;
     BEGIN
-        SELECT * BULK COLLECT INTO t_resvations FROM RESERVATIONS WHERE RESERVATIONS.ID_MEMBRE = p_id_membre;
+        SAVEPOINT avant_modification_archive;
 
-        SELECT * BULK COLLECT INTO t_paiements FROM PAIMENTS WHERE PAIMENTS.ID_MEMBRE = p_id_membre;
+        SELECT ID_RESERVATION, DATE_RESERVATION, ID_MEMBRE, ID_ACTIVITE, SYSDATE BULK COLLECT INTO t_resvations FROM RESERVATIONS WHERE RESERVATIONS.ID_MEMBRE = p_id_membre;
+
+        SELECT ID_PAIMENT, ID_MEMBRE, MONTANT, DATE_PAIMENT, TYPE_PAIMENT, SYSDATE BULK COLLECT INTO t_paiements FROM PAIMENTS WHERE PAIMENTS.ID_MEMBRE = p_id_membre;
 
         SELECT * INTO v_membre FROM MEMBRES WHERE MEMBRES.ID_MEMBRE = p_id_membre;
 
         FORALL i IN t_resvations.FIRST .. t_resvations.LAST
             INSERT INTO RESERVATIONS_ARCHIVEES (ID_RESERVATION, DATE_RESERVATION, ID_MEMBRE, ID_ACTIVITE, DATE_ARCHIVAGE)
-            VALUES(SEQ_GENERATEUR_ID.nextval, t_resvations(i).DATE_RESERVATION, t_resvations(i).ID_MEMBRE, t_resvations(i).ID_ACTIVITE, SYSDATE);
+            VALUES(t_resvations(i).ID_RESERVATION, t_resvations(i).DATE_RESERVATION, t_resvations(i).ID_MEMBRE, t_resvations(i).ID_ACTIVITE, SYSDATE);
 
         FORALL i IN t_paiements.FIRST .. t_paiements.LAST
             INSERT INTO PAIEMENTS_ARCHIVES (ID_PAIEMENT, ID_MEMBRE, MONTANT, DATE_PAIEMENT, TYPE_PAIEMENT, DATE_ARCHIVAGE)
-            VALUES(SEQ_GENERATEUR_ID.nextval, t_paiements(i).ID_MEMBRE, t_paiements(i).MONTANT, t_paiements(i).DATE_PAIMENT, t_paiements(i).TYPE_PAIMENT, SYSDATE);
+            VALUES(t_paiements(i).ID_PAIEMENT, t_paiements(i).ID_MEMBRE, t_paiements(i).MONTANT, t_paiements(i).DATE_PAIEMENT, t_paiements(i).TYPE_PAIEMENT, SYSDATE);
 
-        INSERT INTO MEMBRES_ARCHIVES VALUES(SEQ_GENERATEUR_ID.nextval, v_membre.NOM_MEMBRE, v_membre.PRENOM_MEMBRE, v_membre.EMAIL_MEMBRE, v_membre.TELEPHONE_MEMBRE, v_membre.DATE_INSCRIPTION, v_membre.ID_ABONNEMENT, SYSDATE);
+        INSERT INTO MEMBRES_ARCHIVES VALUES(v_membre.ID_MEMBRE, v_membre.NOM_MEMBRE, v_membre.PRENOM_MEMBRE, v_membre.EMAIL_MEMBRE, v_membre.TELEPHONE_MEMBRE, v_membre.DATE_INSCRIPTION, v_membre.ID_ABONNEMENT, SYSDATE);
 
         FORALL i IN t_resvations.FIRST .. t_resvations.LAST
             DELETE RESERVATIONS WHERE ID_RESERVATION = t_resvations(i).ID_RESERVATION;
 
         FORALL i IN t_paiements.FIRST .. t_paiements.LAST
-            DELETE  PAIMENTS WHERE ID_PAIMENT = t_paiements(i).ID_PAIMENT;
+            DELETE PAIMENTS WHERE ID_PAIMENT = t_paiements(i).ID_PAIEMENT;
 
         COMMIT;
     EXCEPTION
         WHEN NO_DATA_FOUND THEN
+            ROLLBACK TO SAVEPOINT avant_modification_archive;
             DBMS_OUTPUT.PUT_LINE('aucune information n''a pas été trouver avec l''id donner');
         WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('une erreur est survenue l"aure de l"execution de la procedure archiver_client_et_dependances');
+            ROLLBACK;
+            DBMS_OUTPUT.PUT_LINE('une erreur est survenue l"aure de l"execution de la procedure archiver_client_et_dependances' || SQLERRM);
     END archiver_membre_et_dependances;
 
 END pkg_gestion_securisee_des_membres;
